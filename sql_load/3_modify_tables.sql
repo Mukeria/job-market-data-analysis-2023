31037fc890034ec345ccf17140925e23ffef0277
Mukeria <mukeraps@gmail.com>
2026-05-01 01:19:19 -0400

    Update 3_modify_tables.sql

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -30,13 +30,13 @@ FROM 'C:\Users\Home\Desktop\csv_files\company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM 'C:\Users\Home\Desktop\csv_files\skills_dim.csv'
+FROM 'File path'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM 'C:\Users\Home\Desktop\csv_files\job_postings_fact.csv'
+FROM 'File path'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM 'C:\Users\Home\Desktop\csv_files\skills_job_dim.csv'
+FROM 'File path'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
